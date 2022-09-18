b917d55bceae1a9cce18efe02a58c814c3a5087a
Tona <tona@mango-byte.com>
2022-09-18 16:05:48 +0700

    init php e-learning

---
+++ tests/HelpersTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HelpersTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test()
+    {
+        $this->assertTrue(str_contains('Laravel', 'Lara'));
+        $this->assertTrue(true);
+    }
+}
