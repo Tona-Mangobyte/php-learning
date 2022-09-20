49c3a2badc947cf080ad7e7c6ffb8aeb3f56ed40
Tona <tona@mango-byte.com>
2022-09-20 15:07:20 +0700

    add some funcs | arr

--- tests/ArrKeysTest.php
+++ tests/ArrKeysTest.php
@@ -84,4 +84,77 @@ class ArrKeysTest extends TestCase
         $this->assertTrue($hasVolvo);
         $this->assertFalse($hasLexus);
     }
+
+    /** @test */
+    public function testArrayKeys() {
+        $cars=array("Volvo"=>"XC90","BMW"=>"X5");
+
+        $keys = array_keys($cars);
+        $this->assertEquals('Volvo', $keys[0]);
+        $this->assertEquals('BMW', $keys[1]);
+    }
+
+    /** @test */
+    public function testArrayProduct() {
+        $a=array(5,5,2,10);
+        $result = array_product($a);
+        $this->assertEquals(500, $result);
+    }
+
+    /** @test */
+    public function testArraySum() {
+        $a=array(5,15,25);
+        $a2=array("a"=>52.2,"b"=>13.7,"c"=>0.9);
+
+        $result = array_sum($a);
+        $result2 = array_sum($a2);
+
+        $this->assertEquals(45, $result);
+        $this->assertEquals(66.8, round($result2, 2));
+    }
+
+    /** @test */
+    public function testArrayCountValues() {
+        $a=array("A","Cat","Dog","A","Dog");
+        $result = array_count_values($a);
+
+        $this->assertEquals(2, $result["A"]);
+        $this->assertEquals(1, $result["Cat"]);
+        $this->assertEquals(2, $result["Dog"]);
+    }
+
+    /** @test */
+    public function testArrayCombine() {
+        $fname=array("Peter","Ben","Joe");
+        $age=array("35","37","43");
+        $result = array_combine($fname,$age);
+
+        $this->assertEquals(35, $result["Peter"]);
+        $this->assertEquals(37, $result["Ben"]);
+        $this->assertEquals(43, $result["Joe"]);
+    }
+
+    /** @test */
+    public function testArrayChunk() {
+        $cars=array("Volvo","BMW","Toyota","Honda","Mercedes","Opel");
+        $result = array_chunk($cars, 2);
+
+        $this->assertEquals('Volvo', $result[0][0]);
+        $this->assertEquals('BMW', $result[0][1]);
+
+        $this->assertEquals('Toyota', $result[1][0]);
+        $this->assertEquals('Honda', $result[1][1]);
+
+        $this->assertEquals('Mercedes', $result[2][0]);
+        $this->assertEquals('Opel', $result[2][1]);
+
+        $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43","Harry"=>"50");
+        $resultAge = array_chunk($age,2,true);
+
+        $this->assertEquals(35, $resultAge[0]["Peter"]);
+        $this->assertEquals(37, $resultAge[0]["Ben"]);
+
+        $this->assertEquals(43, $resultAge[1]["Joe"]);
+        $this->assertEquals(50, $resultAge[1]["Harry"]);
+    }
 }
