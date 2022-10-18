2731cfc8b7fff1f104c3c73f3e5d93c43ec0cb8a
Tona <tona@mango-byte.com>
2022-10-18 11:47:48 +0700

    h

--- src/functions/anonymous.php
+++ src/functions/anonymous.php
@@ -6,3 +6,48 @@ usort($array, function($a, $b) {
 });
 
 print_r($array);
+
+echo "<br/>";
+
+$array2 = array("really long string here, boy", "this", "middling length",
+    "larger");
+$sortOption = 'random';
+
+usort($array2, function($a, $b) use ($sortOption)
+{
+    if ($sortOption == 'random') {
+        // sort randomly by returning (-1, 0, 1) at random
+        return rand(0, 2) - 1;
+    }
+    else {
+        return strlen($a) - strlen($b);
+    }
+});
+
+print_r($array2);
+
+echo "<br/>";
+
+$array3 = array("really long string here, boy", "this", "middling length",
+    "larger");
+$sortOption3 = "random";
+
+function sortNonrandom($array)
+{
+    $sortOption3 = false;
+
+    usort($array, function($a, $b) use ($sortOption3)
+    {
+        if ($sortOption3 == "random") {
+            // sort randomly by returning (-1, 0, 1) at random
+            return rand(0, 2) - 1;
+        }
+        else {
+            return strlen($a) - strlen($b);
+        }
+    });
+
+    print_r($array);
+}
+
+print_r(sortNonrandom($array3));
