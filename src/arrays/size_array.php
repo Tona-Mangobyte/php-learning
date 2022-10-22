aa091cd5d18794abe0d1d571ed388787e281c20b
Tona <tona@mango-byte.com>
2022-10-22 21:56:35 +0700

    size of array in php

---
+++ src/arrays/size_array.php
@@ -0,0 +1,8 @@
+<?php
+$family = array("Fred", "Wilma", "Pebbles");
+$size = count($family); // $size is 3
+print_r($size);
+
+$confusion = array( 10 => "ten", 11 => "eleven", 12 => "twelve");
+$size = count($confusion); // $size is 3
+print_r($size);
