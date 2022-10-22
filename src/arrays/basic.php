36aa5ba070f4791f87be3e606a3d18d4df8a04eb
Tona <tona@mango-byte.com>
2022-10-22 15:44:59 +0700

    add basic array in php

---
+++ src/arrays/basic.php
@@ -0,0 +1,20 @@
+<?php
+$arr = array();
+$arr['id'] = "ST-001";
+$arr['name'] = "Tona Chheun";
+$arr['address'] = "PP";
+print_r($arr);
+
+$arr2 = array(
+  'id' => "ST-001" ,
+  'name' => "Tona Chheun",
+  'address' => "PP",
+);
+print_r($arr2);
+
+$arr3 = [
+    'id' => "ST-001" ,
+    'name' => "Tona Chheun",
+    'address' => "PP",
+];
+print_r($arr3);
