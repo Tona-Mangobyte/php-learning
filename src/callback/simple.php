93e73556cfb67d359617a970ba68b87728dcc405
teatmisy <teatmisyeysan@gmail.com>
2023-06-18 10:30:27 +0700

    add some feature of php

--- src/callback/simple.php
+++ src/callback/simple.php
@@ -1,1 +1,15 @@
 <?php
+
+function basicCallback(Callable $callback) {
+    $callback();
+}
+
+$function = function() {
+    hello();
+};
+function hello() {
+    echo 'Hello, World!' . PHP_EOL;
+}
+
+basicCallback($function); // Hello, World!
+basicCallback("hello"); // Hello, World!
--- src/func_anonymous/simple.php
+++ src/func_anonymous/simple.php
@@ -1,1 +1,21 @@
 <?php
+
+function valuesDefaultIfEmpty($vals) {
+    $fields = [
+        'status' =>  function(&$vals, $name) {
+            $vals[$name] = '01';
+        },
+    ];
+
+    foreach ($fields as $name => $setter_func) {
+        if (!array_key_exists($name, $vals) || empty($vals[$name])) {
+            $setter_func($vals, $name);
+        }
+    }
+    return $vals;
+}
+
+$data = [
+    'status' => null,
+];
+print_r(valuesDefaultIfEmpty($data));
